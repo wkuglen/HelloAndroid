32b916e6d9543006ab9046fc3611eeeba88eaaa8
wkuglen <wkuglen@gmail.com>
2015-02-03 12:17:01 -0600

    Started another Android activity by initiating a response to the send button

--- app/src/main/java/com/wkuglen/myfirstapp/MainActivity.java
+++ app/src/main/java/com/wkuglen/myfirstapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -35,5 +36,10 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /* Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        //Will add something soon
     }
 }
