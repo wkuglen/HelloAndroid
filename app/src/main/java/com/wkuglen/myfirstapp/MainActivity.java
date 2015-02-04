64f1070ad56012bac607117b22cb85d3c1144e72
wkuglen <wkuglen@gmail.com>
2015-02-03 20:25:46 -0600

    Created the Second Activity.  Fixed an error in /java/.../DisplayMessageActivity.java.  Commit of work prior to crash.Next: Receiving the Intent

--- app/src/main/java/com/wkuglen/myfirstapp/MainActivity.java
+++ app/src/main/java/com/wkuglen/myfirstapp/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends ActionBarActivity {
 
     /* Called when the user clicks the Send button */
     public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessa//if button pressgeActivity.class);
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
