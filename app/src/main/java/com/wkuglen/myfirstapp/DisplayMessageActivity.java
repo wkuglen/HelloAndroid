506b4c5aac22de68192b25b7214fcce423504963
wkuglen <wkuglen@gmail.com>
2015-02-06 07:36:47 -0600

    Received the Intent

--- app/src/main/java/com/wkuglen/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/wkuglen/myfirstapp/DisplayMessageActivity.java
@@ -1,5 +1,6 @@
 package com.wkuglen.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,7 +12,9 @@ public class DisplayMessageActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        
     }
 
 
