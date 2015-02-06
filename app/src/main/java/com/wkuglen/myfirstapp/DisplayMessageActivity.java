faa38d901353e7196b8eb839bd035293ad598eb9
wkuglen <wkuglen@gmail.com>
2015-02-06 07:49:22 -0600

    displayed the message. Built the basic Hello World app

--- app/src/main/java/com/wkuglen/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/wkuglen/myfirstapp/DisplayMessageActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class DisplayMessageActivity extends ActionBarActivity {
@@ -12,9 +13,17 @@ public class DisplayMessageActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Get the message from the intent
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        
+        //Create the TextView
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+        //Set the TextView as the activity layout
+        setContentView(textView);
+
+
     }
 
 
